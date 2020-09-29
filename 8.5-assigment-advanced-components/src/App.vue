de070e55620ee6152d51fd6f8f75d28e33805f93
Your Name <you@example.com>
2020-09-29 09:25:29 -0400

    Updated solution

--- 8.5-assigment-advanced-components/src/App.vue
+++ 8.5-assigment-advanced-components/src/App.vue
@@ -3,27 +3,46 @@
     <div class="row">
       <div class="col-xs-12">
         <br />
-        <button class="btn btn-primary">
+        <button
+          class="btn btn-primary"
+          @click="loadedComponent = 'blue-component'"
+        >
           Load Blue Template
         </button>
-        <button class="btn btn-success">
+        <button
+          class="btn btn-success"
+          @click="loadedComponent = 'green-component'"
+        >
           Load Green Template
         </button>
-        <button class="btn btn-danger">
+        <button
+          class="btn btn-danger"
+          @click="loadedComponent = 'red-component'"
+        >
           Load Red Template
         </button>
         <!-- show component based on button clicked -->
         <hr />
+        <component :is="loadedComponent">
+          <p>Blah blah blah</p>
+        </component>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import RedComponent from "./components/Red.vue";
+import BlueComponent from "./components/Blue.vue";
+import GreenComponent from "./components/Green.vue";
+
 // import all the color components
 export default {
-  data: function() {
-    return {};
+  components: { RedComponent, BlueComponent, GreenComponent },
+  data: function () {
+    return {
+      loadedComponent: "BlueComponent",
+    };
   },
 };
 </script>
